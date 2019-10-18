661c9d2e8609a4719a7bfd3beb09098185fd4bc5
khguruge <40533227+khguruge@users.noreply.github.com>
2019-10-18 07:22:39 +0530

    wedlio

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,68 @@
-<h1>Home</h1>
+<div id="slideshow" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="<?php echo $this->config->item('base_url'); ?>/img/assets/wedding.jpg" class="responsive" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo $this->config->item('base_url'); ?>/img/assets/wedding.jpg" class="responsive" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo $this->config->item('base_url'); ?>/img/assets/wedding.jpg" class="responsive" alt="Third slide">
+    </div>
+
+  </div> <!-- carousel-inner -->
+</div> <!-- slideshow -->
+
+<main class="mt-5">
+    <div class="container">
+    <!--Section: Best Features-->
+        <section id="best-features" class="text-center">
+        <!-- Heading -->
+        <h2 class="mb-5 font-weight-bold">Wedding Planning Has Never Been Easier</h2>
+        <div class="row">
+            <!--Grid column-->
+            <div class="col-md-4 mb-5">
+                <img src="<?php echo $this->config->item('base_url'); ?>/img/assets/icon/vision.png" class="responsive" width="150px" height="100" >
+                <h4 class="my-4 font-weight-bold">Wedding Vision</h4>
+                <p>Define your wedding style and get matched with local vendors.</p>
+            </div> <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-4 mb-1">
+                <img src="<?php echo $this->config->item('base_url'); ?>/img/assets/icon/vendor.png" class="responsive" width="150px" height="100">
+                <h4 class="my-4 font-weight-bold">Local Vendors</h4>
+                <p >Find the top-rated local vendors for your style and budget.</p>
+            </div> <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-4 mb-1">
+                <img src="<?php echo $this->config->item('base_url'); ?>/img/assets/icon/budget.png" class="responsive"width="150px" height="100">
+                <h4 class="my-4 font-weight-bold">Budgeter</h4>
+                <p>Get a personalized spending plan based on your unique budget.</p>
+            </div> <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-4 mb-1">
+                <img src="<?php echo $this->config->item('base_url'); ?>/img/assets/icon/checklist.png" class="responsive" width="150px" height="100">
+                <h4 class="my-4 font-weight-bold">Checklist</h4>
+                <p>Always know what to do, when, with your 24/7 wedding planner.</p>
+            </div> <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-4 mb-1">
+                <img src="<?php echo $this->config->item('base_url'); ?>/img/assets/icon/list.png" class="responsive" width="150px" height="100">
+                <h4 class="my-4 font-weight-bold">Vendor List</h4>
+                <p>Streamline your vendor contacts and get pro recommendations.</p>
+            </div> <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-md-4 mb-1">
+                <img src="<?php echo $this->config->item('base_url'); ?>/img/assets/icon/plan.png" class="responsive" width="150px" height="100">
+                <h4 class="my-4 font-weight-bold">Planning Tools</h4>
+                <p>The who, what, when and where of your wedding day in one timeline.</p>
+            </div> <!--Grid column-->
+
+        </div> <!--Grid row-->
+
+    </section> <!--Section: Best Features-->
+</div>
